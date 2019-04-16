d83adbc60fcc3db48825311f316d0b409a92de13
Jon Shier <jon@jonshier.com>
2019-04-16 17:18:11 -0400

    Initial project.

---
+++ AutocompleteTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutocompleteTest
+//
+//  Created by Jon Shier on 4/16/19.
+//  Copyright © 2019 Jon Shier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutocompleteTestTests/AutocompleteTestTests.swift
@@ -0,0 +1,18 @@
+//
+//  AutocompleteTestTests.swift
+//  AutocompleteTestTests
+//
+//  Created by Jon Shier on 4/16/19.
+//  Copyright © 2019 Jon Shier. All rights reserved.
+//
+
+@testable import AutocompleteTest
+import XCTest
+
+
+class AutocompleteTestTests: XCTestCase {
+    func testSomething() {
+    
+    }
+
+}
