382cd9c011a9f12abd9005f8b45db2f4e8d8dd27
Jon Shier <jon@jonshier.com>
2019-05-15 20:06:58 -0400

    CocoaPods 1.7.0.rc2

--- AutocompleteTest/ViewController.swift
+++ AutocompleteTest/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    enum SubType { case one, two }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func doSomething() {
+        
+    }
+}
 
+struct Value {
+    let string: String
 }
 
